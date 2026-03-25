1039c9fac220a3e7c29bec42dc4840028ee5d308
Andy-cccc <229692753@qq.com>
2026-03-26 05:15:43 +1100

    initial commit

---
+++ web/web-admin/src/main/java/com/atguigu/lease/web/admin/custom/config/Knife4jConfiguration.java
@@ -0,0 +1,75 @@
+package com.atguigu.lease.web.admin.custom.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class Knife4jConfiguration {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+
+        return new OpenAPI().info(
+                new Info()
+                        .title("BackendSystemManageAPI")
+                        .version("1.0")
+                        .description("BackendSystemManageAPI"));
+    }
+
+    @Bean
+    public GroupedOpenApi systemAPI() {
+
+        return GroupedOpenApi.builder().group("SystemInformationManage").
+                pathsToMatch(
+                        "/admin/system/**"
+                ).
+                build();
+    }
+
+    @Bean
+    public GroupedOpenApi loginAPI() {
+
+        return GroupedOpenApi.builder().group("BackendLoginManage").
+                pathsToMatch(
+                        "/admin/login/**",
+                        "/admin/info"
+                ).
+                build();
+    }
+
+    @Bean
+    public GroupedOpenApi apartmentAPI() {
+
+        return GroupedOpenApi.builder().group("ApartmentInformationManage").
+                pathsToMatch(
+                        "/admin/apartment/**",
+                        "/admin/room/**",
+                        "/admin/label/**",
+                        "/admin/facility/**",
+                        "/admin/fee/**",
+                        "/admin/attr/**",
+                        "/admin/payment/**",
+                        "/admin/region/**",
+                        "/admin/term/**",
+                        "/admin/file/**"
+                ).build();
+    }
+    @Bean
+    public GroupedOpenApi leaseAPI() {
+        return GroupedOpenApi.builder().group("RentInformationManage").
+                pathsToMatch(
+                        "/admin/appointment/**",
+                        "/admin/agreement/**"
+                ).build();
+    }
+    @Bean
+    public GroupedOpenApi userAPI() {
+        return GroupedOpenApi.builder().group("PlatformCustomerManage").
+                pathsToMatch(
+                        "/admin/user/**"
+                ).build();
+    }
+}
